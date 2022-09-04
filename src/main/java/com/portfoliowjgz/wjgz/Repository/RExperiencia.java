34d1c4cf6b818f70bfbcba4ff08f2d99708e731d
winstonjgz <winstonjgz@yahoo.com>
2022-09-04 14:55:46 -0300

    Actualizacion BE Experiencia

---
+++ src/main/java/com/portfoliowjgz/wjgz/Repository/RExperiencia.java
@@ -0,0 +1,13 @@
+
+package com.portfoliowjgz.wjgz.Repository;
+
+import com.portfoliowjgz.wjgz.Entity.Experiencia;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RExperiencia extends JpaRepository<Experiencia, Integer>{
+ public Optional<Experiencia> findByNombreExp(String nombreExp);
+ public boolean existsByNombreExp(String nombreExp);   
+}
